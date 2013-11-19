834639d0050de23476c27828f5b02e6a73f6e7a3
JMBGaming <justin@jmbgaming.com>
2013-11-19 13:36:17 -0500

    Create installer_foot.php

---
+++ installer_foot.php
@@ -0,0 +1,23 @@
+<?php
+if (!defined('MONO_ON')) {
+	exit;
+}
+?>
+</td>
+<td class="rgrad"></td>
+</tr>
+<tr>
+    <td colspan="3">
+        <table cellpadding="0" cellspacing="0" border="0" width="100%">
+            <tr>
+                <td class="dgradl">&nbsp;</td>
+                <td class="dgrad">&nbsp;</td>
+                <td class="dgradr">&nbsp;</td>
+            </tr>
+        </table>
+    </td>
+</tr>
+</table>
+</center>
+</body>
+</html>
