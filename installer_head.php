a7b6bce4be0dcac0159b99fa5f4aa711f0864a55
JMBGaming <justin@jmbgaming.com>
2013-11-19 13:37:13 -0500

    Create installer_head.php

---
+++ installer_head.php
@@ -0,0 +1,29 @@
+<?php
+if (!defined('MONO_ON')) {
+	exit;
+}
+ob_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Mccodes Version 2.0.5b</title>
+<link href="css/installer.css" type="text/css" rel="stylesheet" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+<body>
+    <center>
+        <table width="970" border="0" cellpadding="0" cellspacing="0"
+            class="table2">
+            <tr>
+                <td class="lgrad"></td>
+                <td class="center"><img src="title.jpg"
+                    alt="Mccodes Version 2" /><br /> <!-- Begin Main Content -->
+                    <h2>Installer</h2> <span
+                    style='color: red; font-weight: bold;'> If
+                        you bought this from anywhere else but <a
+                        href='http://www.mccodes.com' title='MCCodes'>
+                            MCCodes.com </a> please click <a
+                        href='http://www.mccodes.com/contact.php'
+                        title='Contact us'> here </a>.</span> <br /> Thank you
+                    for choosing MCCodes.
