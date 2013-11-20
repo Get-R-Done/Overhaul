df763f6f41fd04c1b396d9a95cc16fd08866ff49
JMBGaming <justin@jmbgaming.com>
2013-11-19 23:17:10 -0500

    Update announcements.php

--- Upload/announcements.php
+++ Upload/announcements.php
@@ -1,14 +1,12 @@
 <?php
-require_once('globals.php');
+include(__DIR__ . '/globals.php');
 $ac = $ir['new_announcements'];
 $q  = $db->query("SELECT `a_text`, `a_time` FROM `announcements` " . "ORDER BY `a_time` DESC");
-echo '
-<table width="80%" cellspacing="1" cellpadding="1" class="table">
+echo '<table width="80%" cellspacing="1" cellpadding="1" class="table">
 		<tr>
 	<th width="30%">Time</th>
 	<th width="70%">Announcement</th>
-		</tr>
-   ';
+		</tr>';
 while ($r = $db->fetch_row($q)) {
 	if ($ac > 0) {
 		$ac--;
@@ -16,13 +14,10 @@ while ($r = $db->fetch_row($q)) {
 	} else {
 		$new = '';
 	}
-	$r['a_text'] = nl2br($r['a_text']);
-	echo '
-		<tr>
-	<td valign=top>' . date('F j Y, g:i:s a', $r['a_time']) . $new . '</td>
-	<td valign=top>' . $r['a_text'] . '</td>
-		</tr>
-   ';
+	echo '<tr>
+	<td>' . date('F j Y, g:i:s a', $r['a_time']) . $new . '</td>
+	<td>' . nl2br($r['a_text']) . '</td>
+		</tr>';
 }
 $db->free_result($q);
 echo '</table>';
